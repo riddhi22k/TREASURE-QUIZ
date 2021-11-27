999d495e192c790eb501534b460699c9cd3f53df
riddhi22k <84701379+riddhi22k@users.noreply.github.com>
2021-11-27 22:47:33 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "GRAHAM BELL";
+const accessCode2 = "CHARLES BABBAGE";
+const accessCode3 = "SERGEY BRIN";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("H M A A R G  E L B L  ", 100,50)
+    fill("lightblue")
+    text("Hint: Invented telephone !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("E L A C S H R    A B B E A G B", 700,150)
+    fill("lightblue")
+    text("Hint: Invented computer for first time!!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("E G S Y R E   B N I R", 100,250)
+    fill("lightblue")
+    text("Hint: Founder of google !!", 100,270)
+
+}
